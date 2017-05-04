2e5a21927af524ad14d0e0f4a051ac5b53e2a0ac
TRQ1 <trq1.lee@gmail.com>
2017-05-04 11:15:29 -0400

    update calculate_date's function

--- images.py
+++ images.py
@@ -16,6 +16,32 @@ def convert_bi_size(size):
     elif gb:
         return '{0:.2f} GB'.format(byte / gb)
 
+def calculate_date(created):
+    format_type = '%d%m%Y:%H:%M:%S'
+    today = datetime.datetime.today().timetuple()
+    created_date = datetime.datetime.fromtimestamp(created).timetuple()
+    start = '{0}{1}{2}:{3}:{4}:{5}'.format(created_date.tm_mday, created_date.tm_mon, created_date.tm_year,
+                             created_date.tm_hour, created_date.tm_min, created_date.tm_sec)
+    end = '{0}{1}{2}:{3}:{4}:{5}'.format(today.tm_mday, today.tm_mon, today.tm_year, today.tm_hour, today.tm_min, today.tm_sec)
+    start_date = datetime.datetime.strptime(start, format_type)
+    end_date = datetime.datetime.strptime(end, format_type)
+    subtraction_date = int((end_date - start_date).total_seconds())
+
+    if subtraction_date < 0:
+        raise ValueError('This date is worng')
+    elif 0 <= subtraction_date <= 60:
+        return '{0} seconds ago'.format(subtraction_date)
+    elif 60 < subtraction_date <= 3600:
+        return '{0} minutes ago'.format(subtraction_date//60)
+    elif 3600 < subtraction_date <= 86400:
+        return '{0} hours ago'.format(subtraction_date//3600)
+    elif 86400 < subtraction_date <= 604800:
+        return '{0} days ago'.format(subtraction_date//86400)
+    elif 604800 < subtraction_date <= 2419200:
+        return '{0} weeks ago'.format(subtraction_date//604800)
+    else:
+        return '{0} months ago'.format(subtraction_date//2419200)
+
 def connect_api(url):
     return docker.APIClient(base_url=url)
 
@@ -30,10 +56,9 @@ def list_images(client):
    for images_info in client.images():
        (repo, tag) = images_info['RepoTags'][0].split(':')
        id = images_info['Id'].split(':')[1][0:12]
-       created = datetime.datetime.fromtimestamp(images_info['Created']).strftime('%d')
+       created = calculate_date(images_info['Created'])
        size = convert_bi_size(images_info['Size'])
        image_data = {'repo':repo, 'tag':tag, 'id':id, 'created':created, 'size':size}
-
        make_row(image_data)
 
 if __name__ == '__main__':
